dd5d06ea7832260950c0cef4892ee944ccd8c19b
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 19:40:11 -0400

    Create EC2_Random_Generator_Fundamentals.py

---
+++ EC2_Random_Generator_Fundamentals.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+print('''
+
+
+ $$$$$\                                               $$$$$$$$\                    $$\       
+   \__$$ |                                              $$  _____|                   \__|      
+      $$ | $$$$$$\   $$$$$$$\ $$\   $$\  $$$$$$$\       $$ |      $$$$$$\  $$\   $$\ $$\       
+      $$ |$$  __$$\ $$  _____|$$ |  $$ |$$  _____|      $$$$$\   $$  __$$\ $$ |  $$ |$$ |      
+$$\   $$ |$$$$$$$$ |\$$$$$$\  $$ |  $$ |\$$$$$$\        $$  __|  $$ /  $$ |$$ |  $$ |$$ |      
+$$ |  $$ |$$   ____| \____$$\ $$ |  $$ | \____$$\       $$ |     $$ |  $$ |$$ |  $$ |$$ |      
+\$$$$$$  |\$$$$$$$\ $$$$$$$  |\$$$$$$  |$$$$$$$  |      $$$$$$$$\\$$$$$$$ |\$$$$$$  |$$ |      
+ \______/  \_______|\_______/  \______/ \_______/       \________|\____$$ | \______/ \__|      
+                                                                 $$\   $$ |                    
+                                                                 \$$$$$$  |                    
+                                                                  \______/                                                                                                                               
+      
+      ''')
+name_quantity = int(input('How many EC2 instances would you like to create names for? '))
+department_name = input('What department do you work for? ').capitalize()
+
+for _ in range(name_quantity):
+    random_numbers = random.randint(10000, 90000)
+    instance_name = f"{department_name}-{random_numbers}"
+    print(instance_name)
