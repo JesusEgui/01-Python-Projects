9b84d01ea49859887fb3088b02b76d4d43e7e18e
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-05 19:38:08 -0400

    Update and rename README.md to EC2_Random_Generator_Advanced.py

---
+++ EC2_Random_Generator_Advanced.py
@@ -0,0 +1,30 @@
+import random
+import string
+
+print('''
+
+   $$$$$\                                               $$$$$$$$\                    $$\       
+   \__$$ |                                              $$  _____|                   \__|      
+      $$ | $$$$$$\   $$$$$$$\ $$\   $$\  $$$$$$$\       $$ |      $$$$$$\  $$\   $$\ $$\       
+      $$ |$$  __$$\ $$  _____|$$ |  $$ |$$  _____|      $$$$$\   $$  __$$\ $$ |  $$ |$$ |      
+$$\   $$ |$$$$$$$$ |\$$$$$$\  $$ |  $$ |\$$$$$$\        $$  __|  $$ /  $$ |$$ |  $$ |$$ |      
+$$ |  $$ |$$   ____| \____$$\ $$ |  $$ | \____$$\       $$ |     $$ |  $$ |$$ |  $$ |$$ |      
+\$$$$$$  |\$$$$$$$\ $$$$$$$  |\$$$$$$  |$$$$$$$  |      $$$$$$$$\\$$$$$$$ |\$$$$$$  |$$ |      
+ \______/  \_______|\_______/  \______/ \_______/       \________|\____$$ | \______/ \__|      
+                                                                 $$\   $$ |                    
+                                                                 \$$$$$$  |                    
+                                                                  \______/                                                                                                
+              
+               ''')
+allowed_departments = ('Marketing', 'marketing', 'Accounting', 'accounting', 'FinOps', 'finops', 'Finops')
+department_name = input('What department do you work for? ').capitalize()
+
+if department_name not in allowed_departments:
+    print("Sorry, you are not allowed to use this EC2 name generator. Please contact I.T. for further info")
+else:
+    name_quantity = int(input('How many EC2 instances would you like to create names for? '))
+
+    for _ in range(name_quantity):
+        random_numbers = random.randint(100000, 900000)
+        instance_name = f"{department_name}-{random_numbers}"
+        print(instance_name)
