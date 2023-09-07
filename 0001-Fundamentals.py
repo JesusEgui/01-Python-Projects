b58e1e9796d314ce583d523067480ba6975ad2c6
Jesus Egui <140346902+JesusEgui@users.noreply.github.com>
2023-09-07 11:36:53 -0400

    Update 0001-Fundamentals.py

--- 0001-Fundamentals.py
+++ 0001-Fundamentals.py
@@ -41,3 +41,39 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+-------------------------------------------------------------------------------------------------------------
+import random
+import string
+
+# Function to generate unique EC2 names
+def generate_ec2_names(num_instances, department):
+    ec2_names = []
+
+    for _ in range(num_instances):
+        # Generate random characters and numbers for the unique name
+        random_chars = ''.join(random.choices(string.ascii_letters, k=3))
+        random_numbers = ''.join(random.choices(string.digits, k=3))
+
+        # Combine department, random characters, and numbers for the name
+        ec2_name = f"{department}-{random_chars}-{random_numbers}"
+
+        ec2_names.append(ec2_name)
+
+    return ec2_names
+
+# Main program
+def main():
+    print("FOUNDATIONAL EC2 Random Name Generator")
+    num_instances = int(input("How many EC2 instances do you want to name? "))
+    department = input("Enter your department name: ")
+
+    generated_names = generate_ec2_names(num_instances, department)
+
+    if generated_names:
+        print("Generated EC2 names:")
+        for name in generated_names:
+            print(name)
+
+if __name__ == "__main__":
+    main()
